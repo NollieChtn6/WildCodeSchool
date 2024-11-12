52a41d7c9aece1d75a9b82e8c8a3e3503dc2ec4a
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-12 15:32:23 +0100

    initialize ApolloClient

--- Quests/GraphQL/GraphQL_Basics/spacex/src/main.tsx
+++ Quests/GraphQL/GraphQL_Basics/spacex/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+	uri: "https://spacex-production.up.railway.app/",
+	cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")!).render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+);
