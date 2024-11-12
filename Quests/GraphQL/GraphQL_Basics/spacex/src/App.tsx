40c96cf03fadce1bca00439d8b4b87afabdb64be
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-12 15:37:21 +0100

    import gql and create GET_LAUNCHES query

--- Quests/GraphQL/GraphQL_Basics/spacex/src/App.tsx
+++ Quests/GraphQL/GraphQL_Basics/spacex/src/App.tsx
@@ -1,4 +1,23 @@
+import { useQuery, gql } from "@apollo/client";
+
 import "./App.css";
+
+const GET_LAUNCHES = gql`
+  query GetLaunches {
+    launches(limit: 5) {
+      id
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
 
 function App() {
 	return <h1>Hello World</h1>;
