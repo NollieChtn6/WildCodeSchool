f93b554720744fb5c7c76eb34969c14ceb2c4991
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-12 17:28:39 +0100

    fix: 'forbidden non-null assertion' error

--- Quests/GraphQL/01_GraphQL_Basics/spacex/src/main.tsx
+++ Quests/GraphQL/01_GraphQL_Basics/spacex/src/main.tsx
@@ -4,12 +4,15 @@ import App from "./App.tsx";
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement as HTMLElement);
+
 const client = new ApolloClient({
 	uri: "https://spacex-production.up.railway.app/",
 	cache: new InMemoryCache(),
 });
 
-createRoot(document.getElementById("root")!).render(
+root.render(
 	<ApolloProvider client={client}>
 		<App />
 	</ApolloProvider>,
