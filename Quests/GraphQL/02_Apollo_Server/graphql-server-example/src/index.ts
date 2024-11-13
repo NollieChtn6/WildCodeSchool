6b7d11cc387c80618184142b501f7aa11acdf3b7
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-13 14:26:41 +0100

    reformat file

--- Quests/GraphQL/02_Apollo_Server/graphql-server-example/src/index.ts
+++ Quests/GraphQL/02_Apollo_Server/graphql-server-example/src/index.ts
@@ -4,17 +4,17 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 const books: Book[] = [
-	{
-		id: "0",
-		title: "The Awakening",
-		author: "Kate Chopin",
-	},
+  {
+    id: "0",
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
 
-	{
-		id: "1",
-		title: "City of Glass",
-		author: "Paul Auster",
-	},
+  {
+    id: "1",
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
 ];
 
 const typeDefs = `#graphql
@@ -36,40 +36,32 @@ const typeDefs = `#graphql
 
 `;
 
-@InputType()
-class BookInput {
+@ObjectType()
+class Book {
+  @Field()
+  id: string;
+
   @Field()
   title: string;
 
   @Field()
-  author: string
+  author: string;
 }
 
-@ObjectType()
-class Book {
-	@Field()
-	id: string;
-
-	@Field()
-	title: string;
-
-	@Field()
-	author: string;
-}
 @Resolver(Book)
 class BookResolver {
-	@Query(() => [Book])
-	books() {
-		return books;
-	}
+  @Query(() => [Book])
+  books() {
+    return books;
+  }
 
   @Query(() => Book)
-	getBookById(@Arg("id") id: string) {
-		return books.find((book) => book.id === id);
-	}
+  getBookById(@Arg("id") id: string) {
+    return books.find((book) => book.id === id);
+  }
 
   @Mutation(() => Book)
-  addBook(@Arg("data") {title, author}: BookInput) {
+  addBook(@Arg("data") { title, author }: BookInput) {
     const lastId = Number.parseInt(books.at(-1).id, 10);
     const newId = (lastId + 1).toString();
     books.push({
@@ -81,25 +73,32 @@ class BookResolver {
   }
 }
 
+@InputType()
+class BookInput {
+  @Field()
+  title: string;
 
+  @Field()
+  author: string;
+}
 
 const resolvers = {
-	Query: {
-		books: () => books,
-		getBookById: (_, args) => books.find((book) => book.id === args.bookId),
-	},
-	Mutation: {
-		addBook: (_, args) => {
-			const lastId = Number.parseInt(books.at(-1).id, 10);
-			const newId = (lastId + 1).toString();
-			books.push({
-				title: args.title,
-				author: args.author,
-				id: newId,
-			});
-			return books.at(-1);
-		},
-	},
+  Query: {
+    books: () => books,
+    getBookById: (_, args) => books.find((book) => book.id === args.bookId),
+  },
+  Mutation: {
+    addBook: (_, args) => {
+      const lastId = Number.parseInt(books.at(-1).id, 10);
+      const newId = (lastId + 1).toString();
+      books.push({
+        title: args.title,
+        author: args.author,
+        id: newId,
+      });
+      return books.at(-1);
+    },
+  },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
